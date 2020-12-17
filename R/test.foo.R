89a7681f83b7125eab0d07149bde5e08ae71c543
whitneysu <wanning_su@brown.edu>
2020-12-17 00:51:10 -0500

    My commit comments

---
+++ R/test.foo.R
@@ -0,0 +1,11 @@
+#' mean of some vector of numbers
+#' 
+#' @param x A vector of numbers.
+#' @return The mean of numbers in \code{x}.
+#' @examples
+#' test.foo(c(1, 9))
+
+
+test.foo = function(x){
+  print( mean(x) )
+}
